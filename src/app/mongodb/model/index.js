c1a8db697488db1c49a8e9f5ae0c159a5d7f767b
[seongmin_desktop] <thisisclaytoy@naver.com>
2021-07-10 16:40:15 +0900

    controller, router, model 이해 하지만  mongodb연결 실패

---
+++ src/app/mongodb/model/index.js
@@ -0,0 +1,14 @@
+const dbConfig = require('../config/config.js');
+const mongoose = require('mongoose');
+
+//Use Node.js native promise
+mongoose.promise = global.promise;
+
+const db = {}; 
+		db.mongoose = mongoose;
+		db.url = dbConfig.url;
+		db.tutorial = require('./model.js')(mongoose); //이건 형변환인가.
+
+module.exports = db;
+
+
